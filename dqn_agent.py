ff0617525b8283e9a3f90a53b2c07da2dc8e5b2f
Petr Masopust <github@ehp.cz>
2019-12-04 17:27:54 +0100

    Dueling networks, noisy nets, priority replay

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model import QNetwork
+from model import NoisyDuelingQNetwork
 
 import torch
 import torch.nn.functional as F
@@ -34,12 +34,12 @@ class Agent():
         self.seed = random.seed(seed)
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_local = NoisyDuelingQNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_target = NoisyDuelingQNetwork(state_size, action_size, seed).to(device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        self.memory = PrioritizedReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
     
@@ -66,6 +66,8 @@ class Agent():
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
         self.qnetwork_local.eval()
         with torch.no_grad():
+            self.qnetwork_local.sample_noise()
+
             action_values = self.qnetwork_local(state)
         self.qnetwork_local.train()
 
@@ -83,9 +85,11 @@ class Agent():
             experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
             gamma (float): discount factor
         """
-        states, actions, rewards, next_states, dones = experiences
+        states, actions, rewards, next_states, dones, indexes, weights = experiences
 
-        ## TODO: compute and minimize the loss
+        self.qnetwork_target.sample_noise()
+        self.qnetwork_local.sample_noise()
+
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
@@ -94,8 +98,11 @@ class Agent():
         # Get expected Q values from local model
         Q_expected = self.qnetwork_local(states).gather(1, actions)
 
+        # Update memory priorities
+        self.memory.update_priorities(indexes, (Q_expected - Q_targets).detach().squeeze().abs().cpu().numpy().tolist())
+
         # Compute loss
-        loss = F.mse_loss(Q_expected, Q_targets)
+        loss = (F.mse_loss(Q_expected, Q_targets) * weights).mean()
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
@@ -154,6 +161,131 @@ class ReplayBuffer:
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
   
         return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+class PrioritizedReplayBuffer:
+    """Fixed-size prioritized buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed, alpha=0.6, beta_start=0.4, beta_frames=100000):
+        """Initialize a PrioritizedReplayBuffer object.
+
+        Params
+        ======
+            action_size (int): dimension of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+            alpha (float): how much prioritization is used (0 - no prioritization, 1 - full prioritization)
+        """
+        self.memory = []
+        self.priority = []
+
+        self.action_size = action_size
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+
+        self.alpha = alpha
+        self.beta_start = beta_start
+        self.beta_frames = beta_frames
+        self.frame = 1
+
+        self._next_idx = 0
+        self._max_priority = 1.0
+
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+
+    def beta_by_frame(self, frame_idx):
+        return min(1.0, self.beta_start + frame_idx * (1.0 - self.beta_start) / self.beta_frames)
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+
+        idx = self._next_idx
+        priority = self._max_priority ** self.alpha
+
+        if idx >= len(self.memory):
+            self.memory.append(e)
+            self.priority.append(priority)
+        else:
+            self.memory[idx] = e
+            self.priority[idx] = priority
+
+        self._next_idx = (idx + 1) % self.buffer_size
+
+    def _find_prefixsum_idx(self, prefixsum):
+        """Find the highest index `i` in the array such that
+            sum(arr[0] + arr[1] + ... + arr[i - i]) <= prefixsum
+        if array values are probabilities, this function
+        allows to sample indexes according to the discrete
+        probability efficiently.
+        Parameters
+        ----------
+        prefixsum: float
+            upperbound on the sum of array prefix
+        Returns
+        -------
+        idx: int
+            highest index satisfying the prefixsum constraint
+        """
+
+        mi = 0
+        s = self.priority[0]
+        for idx in range(1, len(self.priority)):
+            if s > prefixsum:
+                break
+
+            s += self.priority[idx]
+            mi = idx
+
+        return mi
+
+    def _sample_proportional(self):
+        res = []
+        for _ in range(self.batch_size):
+            mass = random.random() * sum(self.priority[0:len(self.memory) - 1])
+            idx = self._find_prefixsum_idx(mass)
+            res.append(idx)
+        return res
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        indexes = self._sample_proportional()
+
+        weights = []
+
+        # find smallest sampling prob: p_min = smallest priority^alpha / sum of priorities^alpha
+        s = sum(self.priority)
+        p_min = min(self.priority) / s
+
+        beta = self.beta_by_frame(self.frame)
+        self.frame += 1
+
+        # max_weight given to smallest prob
+        max_weight = (p_min * len(self.memory)) ** (-beta)
+
+        for i in indexes:
+            p_sample = self.priority[i] / s
+            weight = (p_sample * len(self.memory)) ** (-beta)
+            weights.append(weight / max_weight)
+        weights = torch.tensor(weights, device=device, dtype=torch.float)
+
+        states = torch.from_numpy(np.vstack([self.memory[i].state for i in indexes])).float().to(device)
+        actions = torch.from_numpy(np.vstack([self.memory[i].action for i in indexes])).long().to(device)
+        rewards = torch.from_numpy(np.vstack([self.memory[i].reward for i in indexes])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([self.memory[i].next_state for i in indexes])).float().to(device)
+        dones = torch.from_numpy(np.vstack([self.memory[i].done for i in indexes]).astype(np.uint8)).float().to(device)
+
+        return (states, actions, rewards, next_states, dones, indexes, weights)
+
+    def update_priorities(self, indexes, priorities):
+        for idx, priority in zip(indexes, priorities):
+            self.priority[idx] = (priority+1e-5) ** self.alpha
+            self._max_priority = max(self._max_priority, (priority+1e-5))
 
     def __len__(self):
         """Return the current size of internal memory."""
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -28,3 +29,147 @@ class QNetwork(nn.Module):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+
+class DuelingQNetwork(nn.Module):
+    """Dueling Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(DuelingQNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.action_size = action_size
+
+        fc1_units=64
+        fc2_units=64
+        fc3_units=32
+        fc4_units=32
+
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+
+        self.fc1_adv = nn.Linear(fc3_units, fc4_units)
+        self.fc2_adv = nn.Linear(fc4_units, action_size)
+
+        self.fc1_val = nn.Linear(fc3_units, fc4_units)
+        self.fc2_val = nn.Linear(fc4_units, 1)
+
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+
+        adv = F.relu(self.fc1_adv(x))
+        val = F.relu(self.fc1_val(x))
+
+        adv = self.fc2_adv(adv)
+        val = self.fc2_val(val).expand(x.size(0), self.action_size)
+
+        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.action_size)
+
+        return x
+
+class NoisyLinear(nn.Module):
+    def __init__(self, in_features, out_features, std_init=0.5):
+        super(NoisyLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.std_init = std_init
+
+        self.weight_mu = nn.Parameter(torch.empty(out_features, in_features))
+        self.weight_sigma = nn.Parameter(torch.empty(out_features, in_features))
+
+        self.bias_mu = nn.Parameter(torch.empty(out_features))
+        self.bias_sigma = nn.Parameter(torch.empty(out_features))
+
+        self.weight_epsilon = torch.empty(out_features, in_features)
+        self.bias_epsilon = torch.empty(out_features)
+
+        self.reset_parameters()
+        self.sample_noise()
+
+    def reset_parameters(self):
+        mu_range = 1.0 / math.sqrt(self.in_features)
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
+        self.bias_mu.data.uniform_(-mu_range, mu_range)
+        self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.out_features))
+
+    def _scale_noise(self, size):
+        x = torch.randn(size)
+        return x.sign().mul_(x.abs().sqrt_())
+
+    def sample_noise(self):
+        epsilon_in = self._scale_noise(self.in_features)
+        epsilon_out = self._scale_noise(self.out_features)
+        self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+        self.bias_epsilon.copy_(epsilon_out)
+
+    def forward(self, inp):
+        if self.training:
+            return F.linear(inp, self.weight_mu + self.weight_sigma * self.weight_epsilon,
+                            self.bias_mu + self.bias_sigma * self.bias_epsilon)
+        else:
+            return F.linear(inp, self.weight_mu, self.bias_mu)
+
+class NoisyDuelingQNetwork(nn.Module):
+    """Dueling Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(NoisyDuelingQNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.action_size = action_size
+
+        fc1_units=64
+        fc2_units=64
+        fc3_units=32
+        fc4_units=32
+
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+
+        self.fc1_adv = NoisyLinear(fc3_units, fc4_units)
+        self.fc2_adv = NoisyLinear(fc4_units, action_size)
+
+        self.fc1_val = NoisyLinear(fc3_units, fc4_units)
+        self.fc2_val = NoisyLinear(fc4_units, 1)
+
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+
+        adv = F.relu(self.fc1_adv(x))
+        val = F.relu(self.fc1_val(x))
+
+        adv = self.fc2_adv(adv)
+        val = self.fc2_val(val).expand(x.size(0), self.action_size)
+
+        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.action_size)
+
+        return x
+
+    def sample_noise(self):
+        self.fc1_adv.sample_noise()
+        self.fc2_adv.sample_noise()
+
+        self.fc1_val.sample_noise()
+        self.fc2_val.sample_noise()
